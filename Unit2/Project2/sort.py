5e799026d55c1b92e2766a6d5dbf512650df851b
wesleyCampbell <wesley.campbell101@gmail.com>
2021-09-24 00:07:32 -0600

    Fixed pivot in _partition. Fixed pylint. Finished

--- Unit2/Project2/sort.py
+++ Unit2/Project2/sort.py
@@ -12,7 +12,7 @@ import random
 import statistics
 
 
-def swap(lyst, a, b):
+def swap(lyst, first, second):
     """
     Swaps two values in a lyst
 
@@ -20,21 +20,21 @@ def swap(lyst, a, b):
     ----------
     lyst : List
         The list where values are being swapped
-    a : Int
+    first : Int
         The first index to be swapped
-    b : Int
+    second : Int
         The second index to be swapped
 
     Returns
     -------
     None
     """
-    lyst[a], lyst[b] = lyst[b], lyst[a]
+    lyst[first], lyst[second] = lyst[second], lyst[first]
 
 
 def is_sorted(lyst):
     """
-    Tests to see if a list is sorted 
+    Tests to see if a list is sorted
     smallest to largest
 
     Paramaters
@@ -72,10 +72,11 @@ def time_it(func):
         Times func
         """
         start = time.perf_counter()
+        print(f"Starting {func.__name__}")
         result = func(*args, **kwargs)
         end = time.perf_counter()
 
-        print(f"{func.__name__} took {end - start} to complete.")
+        print(f"{func.__name__} duration: {end - start} seconds.\n")
         return result
 
     return wrap
@@ -88,8 +89,8 @@ def make_data(size):
     Paramaters:
     -----------
     size : Int
-        The size of the dataset. 
-        The range of the dataset will also be 
+        The size of the dataset.
+        The range of the dataset will also be
         -2 * size to 2 * size.
 
     Yields:
@@ -147,7 +148,10 @@ def insertion_sort(lyst):
         current_value = lyst[i]
         current_index = i
 
+        # While i pointer is less than the length of the list
+        # and the next value is less than the current
         while current_index < len(lyst) - 1 and lyst[current_index + 1] < current_value:
+            # Shift the list down
             lyst[current_index] = lyst[current_index + 1]
             current_index += 1
         lyst[current_index] = current_value
@@ -170,7 +174,7 @@ def mergesort(lyst):
     return _merge_sort(lyst, 0, len(lyst) - 1)
 
 
-def _merge_sort(lyst, a=-1, b=-1):
+def _merge_sort(lyst, pointer_l, pointer_r):
     """
     Performs the merge sort algorithm on lyst.
     This algorithm is recursive. It does not use
@@ -180,25 +184,28 @@ def _merge_sort(lyst, a=-1, b=-1):
     ----------
     lyst : List
         The list being sorted
+    pointer_l : Int
+        The pointer for the beginning of the sub-list
+    pointer_r : Int
+        The pointer for the end of the sub-list
 
     Returns
     -------
     (List): The sorted list
     """
     # If the pointers are the same, exit recursion
-    if a == b:
-        return [lyst[a]]
+    if pointer_l == pointer_r:
+        return [lyst[pointer_l]]
 
-    else:
-        mid = (b - a) // 2 + a
+    mid = (pointer_r - pointer_l) // 2 + pointer_l
 
-        left = _merge_sort(lyst, a, mid)
-        right = _merge_sort(lyst, mid + 1, b)
+    left = _merge_sort(lyst, pointer_l, mid)
+    right = _merge_sort(lyst, mid + 1, pointer_r)
 
-        return _merge(left, right)
+    return _merge(left, right)
 
 
-def _merge(a, b):
+def _merge(list_a, list_b):
     """
     Used in the _merge_sort() function.
     Merges two lists -- a and b -- into one sorted
@@ -206,9 +213,9 @@ def _merge(a, b):
 
     Parameters:
     -----------
-    a : List
+    list_a : List
         The first list
-    b : List
+    list_b : List
         The second list
 
     Returns:
@@ -219,20 +226,26 @@ def _merge(a, b):
     i = 0
     j = 0
     merged = []
-    while i < len(a) and j < len(b):
-        if a[i] < b[j]:
-            merged.append(a[i])
+    # Iterate through both lists.
+    # If the value in the first list is
+    # Less than that of the second, add it to
+    # merged; and vice-versa.
+    while i < len(list_a) and j < len(list_b):
+        if list_a[i] < list_b[j]:
+            merged.append(list_a[i])
             i += 1
         else:
-            merged.append(b[j])
+            merged.append(list_b[j])
             j += 1
 
-    while i < len(a):
-        merged.append(a[i])
+    # Add the remaining values in the first list to merged
+    while i < len(list_a):
+        merged.append(list_a[i])
         i += 1
 
-    while j < len(b):
-        merged.append(b[j])
+    # Add the remaining values in the second list to merged
+    while j < len(list_b):
+        merged.append(list_b[j])
         j += 1
 
     return merged
@@ -269,11 +282,11 @@ def _quick_sort(lyst, left, right):
     Returns:
     --------
     lyst : List
-        Returns lyst when done. 
+        Returns lyst when done.
     """
     # If the left and right pointers are equal or have passed
     # one another, exit the function. Serves as the base case.
-    if (left < right):
+    if left < right:
         # Partition the sub-list
         pivot = _partition(lyst, left, right)
 
@@ -290,7 +303,7 @@ def _partition(lyst, left, right):
     Used in the _quick_sort() function.
     Used for selecting a pivot value, then
     making all values less than the pivot to
-    the right of the pivot. The values greater 
+    the right of the pivot. The values greater
     than the pivot value go to the right.
 
     Parameters:
@@ -308,9 +321,9 @@ def _partition(lyst, left, right):
         The index of the pivot value
     """
     # Assign the pivot value
-    # pivot = _get_pivot(lyst, left, right)
-    pivot = left
+    pivot = _get_pivot(lyst, left, right)
     swap(lyst, pivot, right)
+    pivot = right
 
     # The pointer for values less than the pivot
     pointer = left - 1
@@ -319,10 +332,13 @@ def _partition(lyst, left, right):
     # than the pointer, swap it with the pointer
     while left < right:
         if lyst[left] <= lyst[pivot]:
+            # Increment the lesser value pointer and swap
             pointer += 1
             swap(lyst, pointer, left)
 
+        # Increment the searching pointer
         left += 1
+    # Swap the pivot value with its proper index
     swap(lyst, pointer + 1, right)
 
     return pointer + 1
@@ -347,33 +363,66 @@ def _get_pivot(lyst, left, right):
     Returns:
     --------
     median : Int
-        The index of the pivot value 
+        The index of the pivot value
     """
+    # The center index of the list
     mid_index = (right - left) // 2 + left
 
-    first_value = (left, lyst[left])
-    last_value = (right, lyst[right])
-    mid_value = (mid_index, lyst[mid_index])
+    first_value = (left, lyst[left])  # The first value in the list
+    last_value = (right, lyst[right])  # The second value in the list
+    mid_value = (mid_index, lyst[mid_index])  # The middle value of the list
 
     values = [first_value, last_value, mid_value]
 
+    # Get the median of the three values and return
     median = statistics.median([x[1] for x in values])
-
     for value in values:
         if value[1] == median:
             return value[0]
+    # Gets pylint off my chest (If there is no median return first value)
+    return first_value[0]
+
+
+def timsort(lyst):
+    """
+    The built-in sorting function in python.
+    Combination of merge-sort and selection-sort.
+
+    Paramaters:
+    -----------
+    lyst : List
+        The list to be sorted
+
+    Returns:
+    --------
+    (list): The sorted list
+    """
+    return sorted(lyst)
 
 
 def main():
     """
-    The main function for sort.py
+    The main function for sort.py.
+    Times the five search algorithms
+        - selection_sort()
+        - insertion_sort()
+        - mergesort()
+        - quicksort()
+        - timsort()
     """
-    data_gen = make_data(15)
+    data_size = 50000
+    data_gen = make_data(data_size)
 
-    data = next(data_gen)
-    sorted_data = quicksort(data)
-
-    print(sorted_data)
+    # Selection sort algorithm
+    time_it(selection_sort)(next(data_gen))
+    # Insertion sort algorithm
+    time_it(insertion_sort)(next(data_gen))
+    # Merge sort algorithm
+    time_it(mergesort)(next(data_gen))
+    # Quick sort algorithm
+    time_it(quicksort)(next(data_gen))
+    # Timsort algorithm
+    time_it(timsort)(next(data_gen))
 
 
 if __name__ == "__main__":

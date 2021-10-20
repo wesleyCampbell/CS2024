8d9824a97d7675542f9774979de85a72b8c6bb50
wesleyCampbell <wesley.campbell101@gmail.com>
2021-10-19 19:55:57 -0600

    Added the data collection in driver

--- Unit3/LinkedLists/main.py
+++ Unit3/LinkedLists/main.py
@@ -2,23 +2,38 @@
 Main driver file for the Linked Lists module
 """
 
-import random
+import os
 from course import Course
 from courselist import CourseList
+
+DATAPATH = "Unit3/LinkedLists/data.txt"
+
+
+def get_data(filepath):
+    """
+    Collects data from a datafile
+    """
+    working_directory = os.getcwd()
+
+    course_list = CourseList()
+    with open(os.path.join(working_directory, filepath), "r") as data:
+        for line in data.readlines():
+            values = line.strip().split(',')
+
+            values[0], values[2], values[3] = int(
+                values[0]), float(values[2]), float(values[3])
+
+            course_list.insert(Course(*values))
+
+    return course_list
 
 
 def main():
     """
     The main function for main.py
     """
-    courselist = CourseList()
-
-    for _ in range(10):
-        value = random.randint(0, 1000)
-        courselist.insert(Course(value))
-
-    for course in courselist:
-        print(course)
+    course_list = get_data(DATAPATH)
+    print(course_list)
 
 
 if __name__ == "__main__":
